5c9797bfe131ddf862f7d757b1a2f2c572ca769c
kedyxa <kedyshkolex@gmail.com>
2022-04-06 11:32:49 +0300

    I made a simple calculator about the help of oop

--- OOP.py
+++ OOP.py
@@ -1,19 +1,61 @@
-# task_1_oop_1
+# # task_1_oop_1
+#
+# class Example:
+#     c = 1
+#     d = 2
+#     def __init__(self,a,b):
+#         self.a = a
+#         self.b = b
+#     def func(self):
+#         self.e = 5
+#         print(self.e)
+#     def func1(self):
+#         return self.c + self.d
+#     def func2(self):
+#         return self.a**self.b
+# ex = Example(int(input()),int(input()))
+# print(ex.func1())
+# print(ex.func2())
+# print(ex.func())
 
-class Example:
-    c = 1
-    d = 2
-    def __init__(self,a,b):
-        self.a = a
-        self.b = b
-    def func(self):
-        self.e = 5
-        print(self.e)
-    def func1(self):
-        return self.c + self.d
-    def func2(self):
-        return self.a**self.b
-ex = Example(int(input()),int(input()))
-print(ex.func1())
-print(ex.func2())
-print(ex.func())
+
+#task_2
+#
+# class Calc:
+#     def __init__(self):
+#         self.func4()
+#
+#     def func(self):
+#         return self.a + self.b
+#
+#     def func1(self):
+#         return self.a - self.b
+#
+#     def func2(self):
+#         return self.a * self.b
+#
+#     def func3(self):
+#         if self.b == 0:
+#             return "error"
+#         else:
+#             return self.a/self.b
+#
+#     def func4(self):
+#         self.a = float(input("введите число"))
+#         self.b = float(input("введите число"))
+#
+# while True:
+#     print("введите выполняемую операцию: "'+','-','*','/')
+#     x = input()
+#     ex = Calc()
+#     if x == '+':
+#         print(ex.func())
+#     elif x == '-':
+#         print(ex.func1())
+#     elif x == '*':
+#         print(ex.func2())
+#     elif x == '/':
+#         print(ex.func3())
+#     else:
+#         break
+#
